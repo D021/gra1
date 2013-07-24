2763f470e51cc958a36139820179a4748fd93e32
Mariusz 'shd' Gliwinski <alienballance@gmail.com>
2013-07-24 21:24:00 +0200

    blender palm

---
+++ Assets/CustomAssets/CharacterController/TreeCutter.js
@@ -0,0 +1,64 @@
+﻿#pragma strict
+
+function Update ()
+{
+	if( Input.GetMouseButtonDown(0) ) // LEFT CLICK
+	{
+		//var terrain = Terrain.activeTerrain;
+		//var trees = terrain.terrainData.treeInstances;
+		var trees: TreeInstance[] = Terrain.activeTerrain.terrainData.treeInstances;
+		
+		var hit : RaycastHit;
+		var fwd = transform.TransformDirection(Vector3.forward);
+		if( Physics.Raycast (transform.position, fwd, hit, 10) )
+		{
+			if( hit.collider.name != Terrain.activeTerrain.name )
+			{
+				return;
+			}
+			
+			/*
+			var sampleHeight = Terrain.activeTerrain.SampleHeight(hit.point);
+			if( hit.point.y <= sampleHeight + 0.01 )
+			{
+				return;
+			}
+			*/
+			
+			var terrain = Terrain.activeTerrain.terrainData;
+			var treeInstances = terrain.treeInstances;
+			
+			var maxDistance = float.MaxValue;
+			var closestTreePos = Vector3.zero;
+			var closestTreeIdx = 0;
+			
+			for(var i=0; i<trees.length; i++)
+			{
+				var currentTree = trees[i];
+				var currentTreeWorldPos = Vector3.Scale(currentTree.position, terrain.size);
+				var distance = Vector3.Distance(currentTreeWorldPos, hit.point);
+				
+				if( distance < maxDistance )
+				{
+					maxDistance = distance;
+					closestTreeIdx = i;
+					closestTreePos = currentTreeWorldPos;
+				}
+			}
+			
+			if( 0 == closestTreeIdx )
+			{
+				return;
+			}
+			Debug.Log("found the tree!");
+			
+			var tmp = new Array(trees);
+			tmp.RemoveAt(closestTreeIdx);
+			Terrain.activeTerrain.terrainData.treeInstances = tmp.ToBuiltin(TreeInstance) as TreeInstance[];
+			
+			var heights = terrain.GetHeights(0,0,0,0);
+			terrain.SetHeights(0,0,heights);
+			//Instantiate(FallingTreePrefab, closestTreePosition, Quaternion.identity);
+		}
+	}
+}
